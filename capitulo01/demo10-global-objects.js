f46f4b2590310ba3d54347ead476c36cb7fa5f4c
0xhelton <0xhelton@gmail.com>
2024-06-09 21:36:59 -0300

    :sparkles: feat: feat: add examples of object methods and global object usage

---
+++ capitulo01/demo10-global-objects.js
@@ -0,0 +1,6 @@
+console.log("keys", Object.keys({ nome: "Erick" }));
+console.log("values", Object.values({ nome: "Erick" }));
+console.log("now", Date.now());
+
+// global
+console.log("global", global);
